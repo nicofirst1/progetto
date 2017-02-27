81f509400de21178183da55f8a862d404b20aac3
Nicolo <nicofirst1@gmail.com>
2017-02-27 17:57:28 +0100

    aggiunte varie cose

--- Processing.py
+++ Processing.py
@@ -2,9 +2,29 @@ import pandas as pd
 import numpy
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.grid_search import GridSearchCV
+
 from Data_analisys import scoring
 
 from Preprocessing import  TEST_DATASET, string2vecCV
+
+
+
+def forestGrid(train_dataset, test_dataset):
+    # prima di tutto salvo le y_train
+    y_train = train_dataset["sentiment"]
+
+    # poi mando il train datasaet nel preprocessing usando il CountVectorizer
+    x_train, x_test = string2vecCV(train_dataset["review"], x_test_str=test_dataset["review"])
+
+    param_grid={'n_estimators':[100,300,500,1000],'criterion':['gini','entropy'],'min_samples_split':[2,4,16,32,64],
+                 'min_samples_leaf':[1,5,10],'bootstrap':[True,False],'oob_score':[True,False]}
+
+    grid=GridSearchCV(RandomForestClassifier,param_grid,n_jobs=-1,verbose=True)
+    grid.fit(x_train,y_train)
+    print(grid.best_estimator_)
+    print(grid.best_score_)
+    print(grid.best_params_)
 
 
 def forest(train_dataset, test_dataset):
@@ -18,6 +38,7 @@ def forest(train_dataset, test_dataset):
     #inizzializzo l'estimatore e inizio il fittaggio
     forest = RandomForestClassifier(n_estimators=300,n_jobs=-1, verbose=1, criterion="entropy")
     forest=forest.fit(x_train,y_train)
+
 
 
     #adesso posso provare a fare la predizione
--- prova.py
+++ prova.py
@@ -1,6 +1,6 @@
 from Data_analisys import *
 from Processing import *
 
-pricipal_component(TRAIN_DATASET)
+#pricipal_component(TRAIN_DATASET)
 
-#forest(TRAIN_DATASET,TEST_DATASET)
+forestGrid(TRAIN_DATASET,TEST_DATASET)
