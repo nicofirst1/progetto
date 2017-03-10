c8c4d5286551f7959ceea29b2bcf09a7fef22f8e
Nicolo <nicofirst1@gmail.com>
2017-03-10 18:00:31 +0100

    aggiunta presentazione fino al tfidf

--- Processing.py
+++ Processing.py
@@ -1,7 +1,9 @@
+import time
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.linear_model import SGDClassifier
 from sklearn.naive_bayes import *
+from sklearn.svm import SVC
 
 from Data_analisys import plot_SGD_vect, plot_forest_vect, plot_SGD_decision
 from Preprocessing import string2vecCV
@@ -10,17 +12,20 @@ from util import scoring, polish_tfidf_kbest, cross_validation_score
 TO_PLOT=True
 
 
-
 def forest_classifier(train_set_labled,train_set_unlabled,test_set):
 
     #tratto i dati
     xtrain_vec, xtest_vec,ytrain=polish_tfidf_kbest(train_set_labled,train_set_unlabled,test_set)
 
     print("inizio classificazione......")
+    start = time.time()
+
 
     # inizzializzo il classificatore e inizio il fittaggio
     forest = RandomForestClassifier(n_estimators=300, n_jobs=-1, verbose=1, criterion="entropy")
     forest = forest.fit(xtrain_vec, ytrain)
+    end = time.time()
+    print("fitting avvenuto\ntempo impiegato: " + str(end - start))
 
     if (TO_PLOT):
         plot_forest_vect(forest)
@@ -42,12 +47,16 @@ def SGD_classifier(train_set_labled,train_set_unlabled,test_set):
     xtrain_vec, xtest_vec,ytrain=polish_tfidf_kbest(train_set_labled,train_set_unlabled,test_set)
 
     print("inizio classificazione......")
+    start = time.time()
 
     #inizzializzo il classificatore e inizio il fittaggio
     sgd=SGDClassifier(verbose=1,n_jobs=-1,loss="modified_huber",random_state=4,n_iter=10,
                       shuffle=True)
 
     sgd.fit(xtrain_vec,ytrain)
+
+    end = time.time()
+    print("fitting avvenuto\ntempo impiegato: " + str(end - start))
 
     if(TO_PLOT):
         plot_SGD_decision(sgd,xtrain_vec,ytrain)
--- prova.py
+++ prova.py
@@ -3,11 +3,11 @@ from sklearn.model_selection import ShuffleSplit
 
 from Data_analisys import plot_learning_curve
 from Preprocessing import TRAIN_DATASET_LABLED, TRAIN_DATASET_UNLABLED
-from Processing import SGD_classifier, forest_classifier
+from Processing import SGD_classifier, forest_classifier, SVC_classifier
 from util import *
 
 ## TRATTAMENTO DATI
-x_train_vec, x_test_vec, vect=string2vecTFIDF(TRAIN_DATASET_LABLED["review"],TRAIN_DATASET_UNLABLED["review"],TEST_DATASET["review"])
+#x_train_vec, x_test_vec, vect=string2vecTFIDF(TRAIN_DATASET_LABLED["review"],TRAIN_DATASET_UNLABLED["review"],TEST_DATASET["review"])
 #
 # plot_chi2_vect(x_train_vec,TRAIN_DATASET_LABLED["sentiment"])
 #
@@ -15,6 +15,7 @@ x_train_vec, x_test_vec, vect=string2vecTFIDF(TRAIN_DATASET_LABLED["review"],TRA
 ## CLASSIFICAZIONE
 #forest_classifier(TRAIN_DATASET_LABLED,TRAIN_DATASET_UNLABLED,TEST_DATASET)
 # SGD_classifier(TRAIN_DATASET_LABLED,TRAIN_DATASET_UNLABLED,TEST_DATASET)
+SVC_classifier(TRAIN_DATASET_LABLED,TRAIN_DATASET_UNLABLED,TEST_DATASET)
 # #naive_bayes(TRAIN_DATASET_LABLED,TEST_DATASET)
 
 ## PLOT SGD
@@ -25,8 +26,8 @@ x_train_vec, x_test_vec, vect=string2vecTFIDF(TRAIN_DATASET_LABLED["review"],TRA
 # grid_search_SGD(xtrain_vec,ytrain)
 
 ## PLOTTING LEARNING RATE
-sgd=SGDClassifier(verbose=1,n_jobs=-1,loss="modified_huber",random_state=4,n_iter=10,shuffle=True)
+#sgd=SGDClassifier(verbose=1,n_jobs=-1,loss="modified_huber",random_state=4,n_iter=10,shuffle=True)
 #forest = RandomForestClassifier(n_estimators=300, n_jobs=-1, verbose=1, criterion="entropy")
 
 #plot_learning_curve(forest,"Forest learning curve",x_train_vec,TRAIN_DATASET_LABLED["sentiment"])
-plot_learning_curve(sgd,"SGD learning curve",x_train_vec,TRAIN_DATASET_LABLED["sentiment"])
+#plot_learning_curve(sgd,"SGD learning curve",x_train_vec,TRAIN_DATASET_LABLED["sentiment"])
